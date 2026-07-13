035ac4b0ecc2a44d6de8b46a16dc46d940b17079
icdem2027 <admin@icdem.org>
2026-07-13 14:50:17 +0530

    update main js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 console.log("iCDEM 2027 Loaded");
+window.addEventListener("scroll",function(){
+
+const navbar=document.querySelector(".navbar");
+
+if(window.scrollY>50){
+
+navbar.classList.add("scrolled");
+
+}
+
+else{
+
+navbar.classList.remove("scrolled");
+
+}
+
+});
